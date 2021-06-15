310735f41d61604e27d4ad14218b2b345f73264a
AyushGaur-1704 <85785196+AyushGaur-1704@users.noreply.github.com>
2021-06-15 12:17:56 +0530

    Add files via upload

---
+++ EndTerm/AyushGaur-1922014/ques.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+class fifth {
+
+		public static void main(String[] args)throws IOException
+		{
+			PrintWriter pw= new PrintWriter("three.txt");
+			BufferedReader br=new BufferedReader(new FileReader("one.txt"));
+			String line= br.readLine();
+			while(line!=null)
+			{
+				pw.println(line);
+				line=br.readLine();
+			}
+			br=new BufferedReader(new FileReader("two.txt"));
+			line=br.readLine();
+			while(line!=null)
+			{
+				pw.println(line);
+				line=br.readLine();
+			}
+			pw.flush();
+			br.close();
+			pw.close();
+			System.out.println("Merged");
+		}
+	}
+
+
